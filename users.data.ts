d3fda7d06b9307a22d259f0b32ee028af6fb3cab
addan <addanzahra@gmail.com>
2024-03-16 20:42:17 -0400

    commit updated update commit commit

--- users.data.ts
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import {promises} from 'fs';
-const fsPromise = promises;
-
-export async function getData() {
-    return await fsPromise.readFile("./data/users.json", "utf-8");
-}
-
